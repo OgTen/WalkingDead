997395b1f6f234a6d41202ce610aa13c72c92dd9
Ten <Masonhunt42851@outlook.com>
2026-08-19 13:14:00 +0100

    items

--- walking_dead.lua
+++ walking_dead.lua
@@ -380,7 +380,7 @@ local ITEM_TYPES = {
         "M1911", "Colt Python", "BerretaM9", "Kar98K", "M1917", "M82A1", "FN Fal",
         "HK UMP-45", "AKS-74U", "Desert Eagle", "FN Five-seveN", "Model77E",
         "M40", "VSS Vintorez", "Mk 2 Grenade", "M67 Grenade", "M18 Smoke Grenade",
-        "SPAS-12", "M16", "Ruger 10/22", "RPK",
+        "SPAS-12", "M16", "Ruger 10/22", "RPK", "M82A1",
     },
     ["Melee"] = {
         "Battle Hammer", "Mace", "Shiv", "Spiked Bat", "Wooden Bat",
